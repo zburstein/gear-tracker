24c99ee47599a2c7f1dd432f2a07c1c337a76d72
zburstein <zburstein@gmail.com>
2018-04-24 13:21:19 -0400

    Prepped sidebar for container component

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import PackListNav from './PackListNav';
 import FontAwesome from 'react-fontawesome';
 
 
@@ -10,12 +9,7 @@ class Sidebar extends Component {
     //TODO create a list of existing gear items that can be placed into a category 
 		return(
 			<div>
-        <h2 className="inline-title">Packs</h2>
-        <button className="add-button add-category" onClick={this.props.createPackList}>
-          <FontAwesome name='plus' size="1x" className="button-icon"/>
-          Add new list
-        </button>
-				<PackListNav packLists={this.props.packLists} setCurrentPackList={this.props.setCurrentPackList} deletePackList={this.props.deletePackList} />
+				<PackList/>
 			</div>
 		)
 	}
