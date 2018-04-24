1aaccc9f44507481cb9aedab825ea0dd2e2f4a46
zburstein <zburstein@gmail.com>
2018-04-24 13:15:46 -0400

    set up app.js for sidebar

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
+import Sidebar from "./components/Sidebar";
 
 
-class App extends Component {
-  render() {
-    return (
-
-      <div className="App">
-
+var App = () => {
+  return (
+    <div className="App">
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-3 sidebar">
+            <Sidebar/>
+          </div>
+        </div>
       </div>
-    );
-  }
+    </div>
+  );
+
 }
 
 export default App;
