efd2eed34a579d510721461c524450cdafcf692c
zburstein <zburstein@gmail.com>
2018-04-25 13:02:55 -0400

    removed tests in index.js

--- client/src/index.js
+++ client/src/index.js
@@ -10,7 +10,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from "./reducers/index"
-import {addPack} from "./actions/packActions"
 
 
 const store = createStore(rootReducer);
@@ -18,10 +17,6 @@ const store = createStore(rootReducer);
 const unsubscribe = store.subscribe(() =>
   console.log(store.getState())
 )
-console.log(store.getState());
-store.dispatch(addPack("Got antoer"));
-console.log(store.getState());
-
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
