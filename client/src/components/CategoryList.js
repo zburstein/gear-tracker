889efe548907132ffcee8376b78afd9af34430fe
zburstein <zburstein@gmail.com>
2018-05-07 15:15:00 -0400

    added some potential TODOs

--- client/src/components/CategoryList.js
+++ client/src/components/CategoryList.js
@@ -3,6 +3,8 @@ import FontAwesome from 'react-fontawesome';
 import Category from "./Category";
 
 const CategoryList = ({categories, currentPack, createCategory, editCategory, updateCategory, deleteCategory, createGearItem}) => {
+  //TODO should I give category its own container that is given the category as props or keep as is
+  //TODO give add gear item its own component?
   return(
     <div>
       {categories.map((category, index) => (
