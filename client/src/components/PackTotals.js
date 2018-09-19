0b671ba5f15398d3452d36804331d2942f417ae0
zburstein <zburstein@gmail.com>
2018-09-19 15:30:28 -0700

    custom legend for chart, stylization, implement rounded convert

--- client/src/components/PackTotals.js
+++ client/src/components/PackTotals.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import convert from 'convert-units';
 import {Doughnut} from 'react-chartjs-2';
+import {roundedConvert} from '../roundedConvert';
+import FontAwesome from 'react-fontawesome';
+
 
 
 function handleSubmit(event){
@@ -12,6 +14,8 @@ function handleSubmit(event){
 }
 
 const PackTotals = ({pack, categories, editPack, updatePack}) => {
+
+  //color for doughnut
   var colorArray = ["blue", "green", "red", "teal", '#FF6633', '#00B3E6', 
       '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
       '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A', 
@@ -22,14 +26,17 @@ const PackTotals = ({pack, categories, editPack, updatePack}) => {
       '#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933',
       '#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3', 
       '#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF'];
+
+  //data for donut
   var data = {
     datasets: [{
-        data: categories.map(c => Math.round(convert(c.weight_in_grams).from("g").to(pack.display_metric) * 100) / 100),
+        data: categories.map(c => roundedConvert(c.weight_in_grams, pack.display_metric)),
         backgroundColor: colorArray
     }],
     labels: categories.map(c => c.name)
   };
 
+  //options for doughnut
   var options = {
     tooltips: {
       callbacks: {
@@ -39,7 +46,7 @@ const PackTotals = ({pack, categories, editPack, updatePack}) => {
       }
     },
     legend: {
-      position: "left"
+      display: false
     }
   };
 
@@ -47,14 +54,49 @@ const PackTotals = ({pack, categories, editPack, updatePack}) => {
     return(
       <form onSubmit={(event) => handleSubmit(event)} onBlur={() => updatePack(pack)}>
         <input className="pack-name" name="name" value={pack.name} onChange={(event) => editPack(pack.id, event)}/>
-        {"Total: " + Math.round( convert(pack.weight_in_grams).from("g").to(pack.display_metric) * 100) / 100}
-        <select name="display_metric" value={pack.display_metric} onChange={(event) => editPack(pack.id, event)}>
-          <option value="g">g</option>
-          <option value="oz">oz</option>
-          <option value="lb">lb(s)</option>
-          <option value="kg">kg</option>
-        </select>
-        <Doughnut data={data} options={options}/>
+        <div className="row">
+
+          
+          <div className="col-4 table">
+
+            {/*Table header*/}
+            <div className="row category-header">
+              <div className="col-7">Category</div>
+              <div className="col-5">Weight</div>
+            </div>
+
+            {/*categories*/}
+            {categories.map((category, index) => (
+              <div key={index} className="row top-dotted">
+                <div className="col-7">
+                  <FontAwesome className="color-ref" name='square' style={{color: colorArray[index]}}/>
+                  {category.name}
+                </div>
+                <div className="col-5 text-right">{`${roundedConvert(category.weight_in_grams, pack.display_metric)} ${pack.display_metric}`}</div>
+              </div>
+            ))}
+
+            {/*Table total*/}
+            <div className="row category-total">
+              <div className="col-7">Total:</div>
+              <div className="col-5 text-right">
+                {roundedConvert(pack.weight_in_grams, pack.display_metric)}
+                <select name="display_metric" value={pack.display_metric} onChange={(event) => editPack(pack.id, event)}>
+                  <option value="g">g</option>
+                  <option value="oz">oz</option>
+                  <option value="lb">lb(s)</option>
+                  <option value="kg">kg</option>
+                </select>
+              </div>
+            </div>
+
+          </div>
+
+
+          <div className="col-8">
+            <Doughnut data={data} options={options}/>
+          </div>
+        </div>
       </form>
     )
   }
