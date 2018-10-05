3ac7b585a2c6acd886c812c16dbd338dd2b5e62b
zburstein <zburstein@gmail.com>
2018-10-05 14:04:59 -0700

    removed unused user actions and reducers

--- client/src/reducers/userReducers.js
+++ client/src/reducers/userReducers.js
@@ -12,8 +12,6 @@ function user(state = initialState, action){
   switch(action.type){
     case "SET_USER":
       return action.user
-    case "SET_UPDATED_AUTH":
-      return (update(state, {"access-token": {$set: action["access-token"]}}))
     default:
       return state
   }
