888e6ef0e615d33e2285f8e88875c3789d976caa
zburstein <zburstein@gmail.com>
2018-10-10 13:51:58 -0700

    switched for to htmlFor and class to className

--- client/src/components/UserForm.js
+++ client/src/components/UserForm.js
@@ -10,25 +10,25 @@ const UserForm = ({login, editForm, userForm, register, toggleFormMode}) => {
     <form onSubmit={(event) => handleSubmit(event, login, userForm, register)}>
       
       {!userForm.session && 
-        <div class="form-group">
-          <label for="name">Name</label>
+        <div className="form-group">
+          <label htmlFor="name">Name</label>
           <input name="name" className="form-control" value={userForm.name} onChange={(event) => editForm(event)}/>
         </div>
       }
 
-      <div class="form-group">
-        <label for="email">Email</label>
+      <div className="form-group">
+        <label htmlFor="email">Email</label>
         <input name="email" className="form-control" value={userForm.email} onChange={(event) => editForm(event)}/>
       </div>
 
-      <div class="form-group">
-        <label for="password">Password</label>
+      <div className="form-group">
+        <label htmlFor="password">Password</label>
         <input name="password" type="password" className="form-control" value={userForm.password} onChange={(event) => editForm(event)}/>
       </div>
 
       {!userForm.session && 
-        <div class="form-group">   
-          <label for="passwordConfirmation">Password Confirmation</label>
+        <div className="form-group">   
+          <label htmlFor="passwordConfirmation">Password Confirmation</label>
           <input name="passwordConfirmation" type="password" className="form-control" value={userForm.passwordConfirmation} onChange={(event) => editForm(event)}/>
         </div>
       }
