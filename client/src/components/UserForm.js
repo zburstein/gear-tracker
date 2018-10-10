2138556bfabe7cf0c9e09ac722faab40358e40f8
zburstein <zburstein@gmail.com>
2018-10-09 17:52:38 -0700

    submit button and conditional text rendering

--- client/src/components/UserForm.js
+++ client/src/components/UserForm.js
@@ -12,9 +12,10 @@ const UserForm = ({login, editForm, userForm, register, toggleFormMode}) => {
       <input name="email" placeholder="email" value={userForm.email} onChange={(event) => editForm(event)}/>
       <input name="password" type="password" placeholder="password" value={userForm.password} onChange={(event) => editForm(event)}/>
       {!userForm.session && <input name="password_confirmation" type="password" placeholder="password confirmation" value={userForm.password} onChange={(event) => editForm(event)}/>}
-      <input class="btn btn-primary" type="submit" value={userForm.session ? "Login" : "Sign Up"}/>
+      <button className="btn btn-primary" type="submit">{userForm.session ? "Login" : "Sign Up"}</button>
       <div>
-        No Account? <span onClick={() => toggleFormMode()}>Signup here</span>
+        <span onClick={() => toggleFormMode()}>{userForm.session ? "Signup here" : "Log in"}</span> 
+
       </div>
     </form>
   )
